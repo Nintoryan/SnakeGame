a231438ee2764266070b88c932eb1374639348c2
Konstantin <k.moskal9901@gmail.com>
2023-08-25 14:39:22 +0300

    new scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const { src, dest, watch, series, parallel } = require("gulp");
+const uglify = require('gulp-uglify');
 const babel = require("gulp-babel");
 const concat = require('gulp-concat');
 
@@ -11,8 +12,36 @@ const buildBundle = ()=> {
     .pipe(babel({
         presets:["@babel/preset-env"]
     }))
+    .pipe(uglify())
     .pipe(dest('public/src'))
    
 }
 
+const replaceLibs = () => {
+    return src([
+        'src/libs/*.js',
+        'src/phaser/*.js'
+    ])
+
+    .pipe(dest('public/src/libs'))
+}
+
+const html = () => {
+    return src([
+        'index.html'
+    ])
+    .pipe(dest('public/src'))
+}
+
+const assets =() =>{
+    return src([
+        'assets/**/*.*'
+    ])
+    .pipe(dest('public/src/assets'))
+}
+
+exports.html = html;
 exports.buildBundle = buildBundle;
+exports.replaceLibs = replaceLibs;
+
+exports.default = series(buildBundle, replaceLibs, assets, html)
