8014b8f891c72eb8547bfbee70660d3167a6dd11
Konstantin <k.moskal9901@gmail.com>
2023-08-25 12:11:21 +0300

    new update

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,6 @@ const { src, dest, watch, series, parallel } = require("gulp");
 const babel = require("gulp-babel");
 const concat = require('gulp-concat');
 
-function js() {
-    return src([
-        "app/assets/js/scripts.js"
-    ])
-        .pipe(babel({
-            presets: ["@babel/preset-env"]
-        }))
-        .pipe(dest("build/assets/js/"))
-}
-
 const buildBundle = ()=> {
     return src([
         'src/*.js'
@@ -21,7 +11,7 @@ const buildBundle = ()=> {
     .pipe(babel({
         presets:["@babel/preset-env"]
     }))
-    .pipe(dest('dist/src'))
+    .pipe(dest('publick/src'))
    
 }
 
